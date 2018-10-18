4878084401470e80dd7716a6d8009df7c1966bae
Stephen <stephen@umw.edu>
2018-10-18 09:56:00 -0400

    First commit.

---
+++ Cool.java
@@ -0,0 +1,15 @@
+
+
+public class Cool {
+    public static void main(String args[]) {
+        try {
+            new Cool().doIt();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void doIt() throws Exception {
+         
+    }
+}
